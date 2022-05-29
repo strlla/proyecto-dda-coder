290cec0457260d022d5d4ab98ce88bdf027ba65c
strlla <estrellaportocarrero@gmail.com>
2022-05-29 13:13:38 -0300

    Add Login and Register screens

--- screens/LoginScreen/index.js
+++ screens/LoginScreen/index.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, TextInput, Button } from "react-native";
 import inputStyles from "../../styles/textInput";
 import buttonStyles from "../../styles/button";
+import { useDispatch } from "react-redux";
+import { signin } from "../../store/actions/auth.action";
+import { COLORS } from "../../assets/constants/colors";
 
 function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const onChangeInput = (value, type) => {
+    if (type === "email") {
+      setEmail(value);
+    }
+    if (type === "password") {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = () => {
+    dispatch(signin(email, password));
+  };
+
   return (
-    <View style={{ backgroundColor: "#FFFFFF" }}>
+    <View style={{ flex: 1, backgroundColor: COLORS.BACKGROUND }}>
       <View style={inputStyles.textInputContainer}>
         <TextInput
           placeholder="Email"
-          onChangeText={() => {}}
+          onChangeText={(value) => onChangeInput(value, "email")}
           style={inputStyles.textInput}
+          keyboardType="email-address"
+          maxLength={60}
+          type="email"
         />
         <TextInput
           placeholder="Password"
           secureTextEntry
-          onChangeText={() => {}}
+          onChangeText={(value) => onChangeInput(value, "password")}
           style={inputStyles.textInput}
         />
       </View>
       <View style={buttonStyles.buttonContainer}>
-        <Button onPress={() => navigation.navigate("Root")} title="Login" color="#6A23B0" />
+        <Button
+          onPress={() => handleSubmit()}
+          title="Login"
+          color={COLORS.PURPLE}
+        />
       </View>
     </View>
   );
--- screens/RegisterScreen/index.js
+++ screens/RegisterScreen/index.js
@@ -1,40 +1,71 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { View, Text, TouchableOpacity, TextInput, Button } from "react-native";
 import styles from "./styles";
 import inputStyles from "../../styles/textInput";
 import buttonStyles from "../../styles/button";
+import { useDispatch } from "react-redux";
+import { COLORS } from "../../assets/constants/colors";
+import { signup } from "../../store/actions/auth.action"
 
 function Register({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+
+  const onChangeInput = (value, type) => {
+    if (type === "email") {
+      setEmail(value);
+    }
+    if (type === "password") {
+      setPassword(value);
+    }
+
+    if (type === "name") {
+      setName(value);
+    }
+  };
+
+  const handleSubmit = () => {
+    dispatch(signup(email, password, name));
+  };
+
   return (
-    <View style={{ backgroundColor: "#FFFFFF", flex: 1 }}>
+    <View style={{ backgroundColor: COLORS.BACKGROUND, flex: 1 }}>
       <View style={inputStyles.textInputContainer}>
         <TextInput
           placeholder="Name"
-          onChangeText={() => {}}
+          onChangeText={(value) => onChangeInput(value, "name")}
           style={inputStyles.textInput}
         />
         <View style={inputStyles.textInputContainer}>
           <TextInput
             placeholder="Email"
-            onChangeText={() => {}}
+            onChangeText={(value) => onChangeInput(value, "email")}
             style={inputStyles.textInput}
           />
           <TextInput
             placeholder="Password"
             secureTextEntry
-            onChangeText={() => {}}
+            onChangeText={(value) => onChangeInput(value, "password")}
             style={inputStyles.textInput}
           />
         </View>
       </View>
       <View style={buttonStyles.buttonContainer}>
-        <Button onPress={() => navigation.navigate("Root")} title="Register" color="#6A23B0" />
+        <Button
+          onPress={() => handleSubmit()}
+          title="Register"
+          color={COLORS.PURPLE}
+        />
       </View>
       <TouchableOpacity
         style={styles.haveAccountButtonStyle}
         onPress={() => navigation.navigate("Login")}
       >
-        <Text style={styles.haveAccountTextStyle}>Already have an account?</Text>
+        <Text style={styles.haveAccountTextStyle}>
+          Already have an account?
+        </Text>
       </TouchableOpacity>
     </View>
   );
