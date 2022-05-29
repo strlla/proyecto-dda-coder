4d4588dfe9fae18c6c4c57e70aae471577dce3de
strlla <estrellaportocarrero@gmail.com>
2022-05-29 13:14:23 -0300

    Add cart

---
+++ screens/CartScreen/index.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, Button, Modal, Pressable } from "react-native";
+import { connect, useSelector, useDispatch } from "react-redux";
+import { confirmCart, removeItem } from "../../store/actions/cart.action";
+import CartItem from "../../components/CartItem";
+import { styles } from "./styles";
+import { COLORS } from "../../assets/constants/colors";
+const Cart = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.cart.items);
+  const total = useSelector((state) => state.cart.total);
+  const user = useSelector((state) => state.auth.userId);
+  const cart = useSelector((state) => state.cart.cart);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleDeleteItem = (id) => dispatch(removeItem(id));
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  useEffect(() => {
+    if (modalVisible) {
+      setTimeout(() => {
+        navigation.navigate("BooksStack");
+      }, 1000);
+    }
+  }, [modalVisible]);
+  const handleConfirmCart = (async) => {
+    setModalVisible(false);
+    dispatch(confirmCart(items, total, user, setModalVisible));
+  };
+
+  const renderItem = ({ item }) => (
+    <CartItem item={item} onDelete={handleDeleteItem} />
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.list}>
+        <FlatList
+          data={items}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderItem}
+        />
+      </View>
+      <Button
+        title="Confirm"
+        onPress={() => handleConfirmCart()}
+        color={COLORS.PURPLE}
+      />
+      <View style={styles.footer}>
+        <Text style={styles.total}>Total:</Text>
+        <Text style={styles.totalPrice}>${total}</Text>
+      </View>
+      <View>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>
+                Your purchase was successful!
+              </Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Ok</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </View>
+  );
+};
+
+export default connect()(Cart);
